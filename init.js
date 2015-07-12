9744e83193f6be2491b795aa37772fb24ce0f488
Damir <damir@withpulp.com>
2015-07-12 18:53:11 -0400

    init orion unstyled package

---
+++ init.js
@@ -0,0 +1,34 @@
+Options.init('homePath');
+Options.init('siteName');
+
+ReactiveTemplates.request('tabs', 'orionTabs');
+
+ReactiveTemplates.set('layout', 'orionLayout');
+ReactiveTemplates.set('outAdminLayout', 'orionOutAdminLayout');
+
+ReactiveTemplates.set('links', 'orionSidebar');
+ReactiveTemplates.set('login', 'orionLogin');
+ReactiveTemplates.set('registerWithInvitation', 'orionRegisterWithInvitation');
+
+ReactiveTemplates.set('myAccount.index', 'orionAccountIndex');
+ReactiveTemplates.set('myAccount.password', 'orionAccountPassword');
+ReactiveTemplates.set('myAccount.profile', 'orionAccountProfile');
+
+ReactiveTemplates.set('accounts.index', 'orionAccountsIndex');
+ReactiveTemplates.set('accounts.update', 'orionAccountsUpdate');
+ReactiveTemplates.set('accounts.create', 'orionAccountsCreate');
+
+ReactiveTemplates.set('configUpdate', 'orionConfigUpdate');
+ReactiveTemplates.set('dictionaryUpdate', 'orionDictionaryUpdate');
+
+// Set the default entity templates
+Options.set('collectionsDefaultIndexTemplate', 'orionCollectionsIndex');
+Options.set('collectionsDefaultCreateTemplate', 'orionCollectionsCreate');
+Options.set('collectionsDefaultUpdateTemplate', 'orionCollectionsUpdate');
+Options.set('collectionsDefaultDeleteTemplate', 'orionCollectionsDelete');
+
+// Pages
+ReactiveTemplates.set('pages.index', 'orionPagesIndex');
+ReactiveTemplates.set('pages.create', 'orionPagesCreate');
+ReactiveTemplates.set('pages.update', 'orionPagesUpdate');
+ReactiveTemplates.set('pages.delete', 'orionPagesDelete');
