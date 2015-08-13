ddd9d50558d405fe2d6d275a3c0c181844ca00e1
Damir <damir@withpulp.com>
2015-08-13 16:06:48 -0400

    updates to pages templates

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dvz:orion-unstyled',
   summary: 'Unstyled admin theme for orion.',
-  version: '0.0.8',
+  version: '0.0.9',
   git: 'https://github.com/amazingBastard/orion-unstyled'
 });
 
